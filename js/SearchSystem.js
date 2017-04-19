416ffa3600e2fa0a00c8f154a10119f24e6eb6f0
NSD <metall112@bk.ru>
2017-04-19 15:43:55 +0300

    fix Import system import)

--- js/SearchSystem.js
+++ js/SearchSystem.js
@@ -3,52 +3,49 @@
  */
 
 
-
-$(document).ready(function () {
-
+window.onload=function () {
 
 
+    $('#autocomplete-input_mobile').on('input', function () {
 
-$('#autocomplete-input_mobile').on('input',function () {
+        var sender = $(this);
+        var text = sender.val();
 
-    var sender = $(this);
-    var text = sender.val();
-
-    if(text == ''|| text == null){
-        $('#search_result_mobile').addClass('hide');
-        $('#mob_company').removeClass('hide');
-        $('#mob_services').removeClass('hide');
-        $('#mob_news').removeClass('hide');
-        $('#mob_career').removeClass('hide');
-        $('#mob_contacts').removeClass('hide');
-    }else{
-        $('#search_result_mobile').removeClass('hide');
-        $('#mob_company').addClass('hide');
-        $('#mob_services').addClass('hide');
-        $('#mob_news').addClass('hide');
-        $('#mob_career').addClass('hide');
-        $('#mob_contacts').addClass('hide');
-    }
-});
-
-
-$('#autocomplete-input').on('input',function () {
-
-    var sender = $(this);
-    var text = sender.val();
-
-    if(text == ''|| text == null){
-
-        $('#search_result').addClass('hide');
-        $('#page_content').removeClass('hide');
-        return false;
-
-    }else{
-        $('#search_result').removeClass('hide');
-        $('#page_content').addClass('hide');
-    }
+        if (text == '' || text == null) {
+            $('#search_result_mobile').addClass('hide');
+            $('#mob_company').removeClass('hide');
+            $('#mob_services').removeClass('hide');
+            $('#mob_news').removeClass('hide');
+            $('#mob_career').removeClass('hide');
+            $('#mob_contacts').removeClass('hide');
+        } else {
+            $('#search_result_mobile').removeClass('hide');
+            $('#mob_company').addClass('hide');
+            $('#mob_services').addClass('hide');
+            $('#mob_news').addClass('hide');
+            $('#mob_career').addClass('hide');
+            $('#mob_contacts').addClass('hide');
+        }
+    });
 
 
+    $('#autocomplete-input').on('input', function () {
 
-});
-});
+        var sender = $(this);
+        var text = sender.val();
+
+        if (text == '' || text == null) {
+
+            $('#search_result').addClass('hide');
+            $('#page_content').removeClass('hide');
+            return false;
+
+        } else {
+            $('#search_result').removeClass('hide');
+            $('#page_content').addClass('hide');
+        }
+
+
+    });
+};
+
