0ed60ea56eb4f76a5f47bcde474552affc7f0820
NSD <metall112@bk.ru>
2017-05-16 11:01:20 +0300

    search system

--- js/SearchSystem.js
+++ js/SearchSystem.js
@@ -52,9 +52,14 @@ window.onload=function () {
 
 
     function handleSearch(inputText) {
+        var preloader="<div class=\"row\">";
+        preloader += "   <div id=\"preloader_div\" class=\" col s12 m12 l12 center center-align\">";
+        preloader += "";
+        preloader += "";
+        preloader += "        <img id=\"preloader\" src=\"images\/bx_loader.gif\">";
+        preloader += "   <\/div> <\/div>";
 
-
-
+        document.getElementById('search_result_div').innerHTML =preloader;
 
 
         var searchComponentUrl = "";
@@ -75,8 +80,14 @@ window.onload=function () {
 
 
                     console.log(jsonD);
+                    parseQueryServices(jsonD);
 
-                    document.getElementById('search_result').innerHTML="hi";
+
+
+
+                   // document.getElementById('search_result_div').innerHTML="";
+
+
 
 
 
@@ -92,4 +103,71 @@ window.onload=function () {
     }
 
 };
+
+function parseQueryServices(data) {
+    var seviceContainer = document.getElementById('search_result_div');
+
+
+    var serviceHtml = "";
+
+    serviceHtml += '<div class=\"row\">';
+
+
+
+    for(var i=0;i<data.length;i++){
+        var j =  data.length-i-1;
+        console.log("elements:" + (i+1) + " other:" + j);
+
+        var elC = i+1;
+
+
+
+
+
+        serviceHtml += "   <div class=\"row col s12 m12 l4\">";
+        serviceHtml += "                <div class=\"col s12 m12 l12\">";
+        serviceHtml += "                    <div class=\"hoverable medium card\">";
+        serviceHtml += "                        <div class=\"card-image\">";
+        serviceHtml += "                            <img class=\"center center-align\" style = \" object-fit: cover;  height:200px;\"  src=\""+loadedServiceElements[i].image+"\">";
+        serviceHtml += "                            <span class=\"card-title center backgr-col center-align \" style=\"font-size: 125%\">"+loadedServiceElements[i].headerText+"<\/span>";
+        serviceHtml += "                        <\/div>";
+        serviceHtml += "                        <div class=\"card-content\">";
+        serviceHtml += "                            <p>"+loadedServiceElements[i].minimalDescription+"<\/p>";
+        serviceHtml += "";
+        serviceHtml += "                        <\/div>";
+        serviceHtml += "                        <div class=\"card-action center-align \">";
+        serviceHtml += "                            <p> <a href=\"javascript:void(0);\" onclick=\""+loadedServiceElements[i].content+"\" class=\"waves-effect col s12 waves-light blue darken-4 btn\">Подробнее<\/a>  <\/p>";
+        serviceHtml += "                        <\/div>";
+        serviceHtml += "                    <\/div>";
+        serviceHtml += "                <\/div>";
+        serviceHtml += "            <\/div>";
+
+
+
+        if(elC%3==0&&j<=3){
+            if(j==3){  }
+            if(j==2){
+                serviceHtml += "<div class=\"col l2\"><\/div>";
+            }
+            if(j==1){
+                serviceHtml += "<div class=\"col l4\"><\/div>";
+            }
+        }
+
+    }
+
+
+
+    serviceHtml += '<\/div>';
+
+
+    console.log(serviceHtml);
+
+    seviceContainer.innerHTML = serviceHtml;
+
+
+
+}
+
+
 
